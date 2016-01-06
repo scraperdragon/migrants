9168e3044d27c4f919f803833b5d520b3ce48924
Dragon Dave McKee <dragon@scraperwiki.com>
2016-01-06 10:53:55 +0000

    XLS output

--- missingmigrants.py
+++ missingmigrants.py
@@ -1,9 +1,22 @@
 import re
+import csv
+
 import requests
 import lxml.html
 import xypath
 
 from StringIO import StringIO
+
+import xlwt
+
+def write_excel(sheets, sheet_names):
+    wb = xlwt.Workbook()
+    for sheet, sheet_name in zip(sheets, sheet_names):
+        ws = wb.add_sheet(sheet_name)
+        for row_num, row in enumerate(sheet):
+            for col_num, cell in enumerate(row):
+                ws.write(row_num, col_num, cell)
+    wb.save('example.xls')
 
 def get_table_from_header(header):
     header_text = header.text_content()
@@ -18,36 +31,43 @@ def get_table_from_header(header):
     # this is a simple case so we could just iterate over the <tr> and <td>
     # elements instead.
 
+def get_table_simple(header):
+    # this won't work if there's colspans/rowspans etc.
+    # but it's OK for this simple case.
+    header_text = header.text_content()
+    year = int(re.search(r"(\d{4})", header_text).group(1))
+
+    table, = header.xpath(".//following::table[1]") # lxml element
+    trs = table.xpath(".//tr")
+    for tr in trs:
+        tds = tr.xpath(".//td|th")
+        values = [td.text_content() for td in tds]
+        yield values
+
 def clean_table(table):
-    return # TODO
     for row in table:
+        row_builder = []
         for cell in row:
-            for char in " ,*":
-                cell.value = cell.value.replace(char, "")
-
-
+            for original, replacement in {u'\xa0': ' ',
+                                          '*': '',
+                                          ',': ''}.items():
+                cell = cell.replace(original, replacement)
+            cell = cell.strip()
+            row_builder.append(cell)
+        yield row_builder
 
 BASEURL = "http://missingmigrants.iom.int/en/latest-global-figures"
 html = requests.get(BASEURL).content
 root = lxml.html.fromstring(html)
 
 headers = root.xpath("//h1")
+headers = [header for header in headers if 'Deaths by month' in header.text_content()]
 
 for header in headers:
-    table = get_table_from_header(header)
-    if table is None:
-        continue
-    clean_table(table)
-
-    for row in table:
-        for cell in row:
-            print cell
-
-
-
-
-
-    print "!!"
+    table = get_table_simple(header)
+    table = clean_table(table)
+    write_excel([table], ["sheet1"])
+    exit()
 
 
 
